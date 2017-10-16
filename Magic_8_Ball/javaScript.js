10bd163248bb9d3e447e77909fdaaea8b390a8d8
learn <learn@learn13.local>
2017-10-16 14:05:05 -0700

    first commit with magic ball done

---
+++ Magic_8_Ball/javaScript.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+$(document).ready(function(){
+  $("#submit").on("click", (function(){
+    var userInput = $(".input").val();
+    console.log(userInput);
+    var answerArray = ["Yes", "No", "Maybe so", "A Billion", "Try again later", "Getting warmer", "WAAAYYYY OFF!", "Absolutely", "Seriously?!?!"]
+    var answer = Math.floor(Math.random()*9);
+    $("#answer").text("The Magic 8 Ball says... " + answerArray[answer]);
+    $(".input").val("");
+  }));
+});
